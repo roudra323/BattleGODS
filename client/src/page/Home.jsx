414c8f5585bd8f2ff61facbcd9f1a8e807817413
roudra323 <asirroudra@gmail.com>
2023-09-16 03:20:35 +0600

    impletented contract interection

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGlobalContext } from "../context";
-import { PageHOC } from "../components";
+import { PageHOC, CustomInput, CustomButton } from "../components";
 
 const Home = () => {
   const { contract, account, isConnected } = useGlobalContext();
+  console.log(contract, account, isConnected);
+  const [playerName, setPlayerName] = useState("");
+
+  const handleClick = async () => {
+    try {
+      if (playerName === "") {
+        alert("Please enter a valid name");
+        return;
+      }
+      if (!isConnected) {
+        alert("Please connect your wallet");
+        return;
+      }
+      const tx = await contract.isPlayer(account);
+      console.log(tx);
+
+      // const tx = await contract.methods
+      //   .registerPlayer(playerName)
+      //   .send({ from: account });
+      // console.log(tx);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div>
-      <h1 className="text-white">
-        {/* {console.log(contract, "\n", account, "\n", isConnected)} */}
-      </h1>
+    <div className="flex flex-col">
+      <CustomInput
+        label="Name"
+        placeholder="Enter your name"
+        value={playerName}
+        handleValueChange={setPlayerName}
+      />
+      <CustomButton
+        title="Register"
+        handleClick={handleClick}
+        restStyles="mt-6"
+      />
     </div>
   );
 };
